1327d79ac298dcd4c6e6e8155c954881d5b7a82d
deloragm <45082601+deloragm@users.noreply.github.com>
2018-11-16 10:15:34 +1100

    Update index.php

--- Crystal Load/index.php
+++ Crystal Load/index.php
@@ -4,7 +4,7 @@
 // You can get a Steam API Key by visiting http://steamcommunity.com/dev/apikey
 // Don't worry about the web address, it won't have any effect so just type in any web site
 // Once you have your steam API Key simply paste the key below. (Make sure the quotation marks are still there or else it won't work)
-$SteamAPIKey = "INSERT YOUR STEAM API KEY HERE";
+$SteamAPIKey = "6ACE2A7842DCC4BB4471EE2F94FBFABB";
 
 
 // Don't edit any of the PHP stuff here or else you may break the script
